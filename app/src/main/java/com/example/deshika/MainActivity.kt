06e2adbe32d844156c614474376ed50ceef13f06
WIC USER <praptickb8@gmail.com>
2024-09-23 08:54:15 -0700

    2nd

--- app/src/main/java/com/example/deshika/MainActivity.kt
+++ app/src/main/java/com/example/deshika/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             DeshikaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Prapti",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
